d62bab31b6f0dc5edd9a26067a43d2c062507f62
willi <wytheli168@163.com>
2020-07-16 09:27:38 +0800

    编写一个Pos组件，并且为其配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Pos from '@/components/page/Pos'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Pos',
+      component: Pos
     }
   ]
 })
